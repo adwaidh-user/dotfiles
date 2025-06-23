dc6f5ae1cd9a28be0fd15144d9cf5fcbf019dff9
adwaidh-user <adwaith3105@gmail.com>
2025-06-23 18:02:16 +0530

    untracked lazu-lock.json and added prettier formatter

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -13,6 +13,7 @@ return {
                 null_ls.builtins.formatting.stylua,
                 null_ls.builtins.formatting.clang_format,
                 null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.prettier,
                 -- linting
                 null_ls.builtins.diagnostics.pylint,
             },
