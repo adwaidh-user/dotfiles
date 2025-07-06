f19614de5badd6ccf297a4c0bceb758fef87cb04
adwaidh-user <adwaith3105@gmail.com>
2025-07-06 18:55:05 +0530

    fixed synatx errors and an alias

--- zsh/shell_config/application_aliases.zsh
+++ zsh/shell_config/application_aliases.zsh
@@ -1,4 +1,3 @@
-alias cat=bat
 alias myip='curl -s -m 5 https://ipleak.net/json/'
 alias timenow='date +"%T"'
 alias datenow='date +"%d-%m-%Y"'
---
+++ zsh/shell_config/edit_conf.zsh
@@ -0,0 +1,24 @@
+source prompt.zsh
+
+edit-conf () {
+    # Checking argument is provided
+    if [[ -z "$1" || $# -gt 2 ]]; then
+        prompt -e "Bad Usage: config [program_name].\n"
+        return 1
+    fi
+
+    if [[ -d "$DOT_DIR/$1" ]]; then
+        cd "$DOT_DIR/$1"
+    else
+        prompt -i "Configuration directory not found in $DOT_DIR for '$1'."
+        prompt -i "start a new config directory for '$1' ? [y/n] (default: n)"
+        read MAKE_NEW_DIR
+
+        if [[ $MAKE_NEW_DIR == "y" ]]; then
+            mkdir -p "$DOT_DIR/$1" && prompt -s "Made config directory for $1 at $DOT_DIR\n"
+            cd "$DOT_DIR/$1"
+        else
+            prompt -i "Exiting...\n"
+        fi
+    fi
+}
