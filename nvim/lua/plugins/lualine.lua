48f36233a056457ab459b8746d50b7f81d2e7fcb
adwaidh-user <adwaith3105@gmail.com>
2025-11-26 14:20:28 +0530

    replaced bufferline with lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,11 +1,23 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-	config = function()
-		require('lualine').setup({
-			options = {
-				theme = 'powerline'
-			}
-		})
-	end
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    options = {
+      theme = 'palenight',  -- use the modified theme table
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { "filename" },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+    tabline = {
+      lualine_a = { "buffers" },
+      lualine_y = { "searchcount", "selectioncount" },
+      lualine_z = { "tabs" },
+    },
+  },
 }
+
