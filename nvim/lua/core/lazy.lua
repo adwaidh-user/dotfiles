32ba41e3421d64fe50225330920b9631304deef0
adwaidh-user <adwaith3105@gmail.com>
2025-12-01 00:15:59 +0530

    removed comments

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -18,11 +18,8 @@ vim.opt.rtp:prepend(lazypath)
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- import your plugins
     { import = "plugins" },
   },
-  -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
   checker = { enabled = true },
 })
